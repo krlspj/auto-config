e5d566d19f0384f05a6c5bb63a5ff1e14822140d
krlspj <43220110+krlspj@users.noreply.github.com>
2021-04-28 22:57:05 +0000

    Create stremio-arch.sh

---
+++ stremio/stremio-arch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+printf "============================\n"
+printf "= Starting Stremio Install =\n"
+printf "= By krls                  =\n"
+printf "============================\n\n"
+
+cd ~/Downloads
+printf "Downloading Stremio --> Flatpak\n\n"
+wget https://dl.flathub.org/repo/appstream/com.stremio.Stremio.flatpakref
+
+printf "installing flatpak \n"
+sudo pacman -Syy
+yes | sudo pacman -S flatpak
+
+printf "Installing stremio...\n\n"
+flatpak install -y com.stremio.Stremio.flatpakref
+printf "==>Launch stremio from flatpak\n\n"
+sleep 1;
+flatpak run com.stremio.Stremio
