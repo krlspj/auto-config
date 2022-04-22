c7aa90d6b133842122d21c102a62fef60c68e8f0
krlspj <43220110+krlspj@users.noreply.github.com>
2022-04-22 23:40:57 +0000

    Update main.sh

--- void-linux/main.sh
+++ void-linux/main.sh
@@ -9,7 +9,15 @@ echo "git config"
 git config --global user.email "krls.pjds@gmail.com"
 git config --global user.name "krlspj"
 
-echo "launch audio_config.sh"
-bash ./audio_config.sh
+#echo "launch audio_config.sh"
+#bash ./audio_config.sh
+echo "installing pipewire and alsa"
+sudo xbps-install -y pipewire alsa-pipewire alsamixer
 
+echo "Setup pipewire config..."
+sudo mkdir -p /etc/alsa/conf.d
+#ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
+#ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
+
+#exec pipewire
 
