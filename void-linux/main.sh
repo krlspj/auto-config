8669166a6fd1a951d7937aeb286c1fd05cd3ea5e
krlspj <43220110+krlspj@users.noreply.github.com>
2022-04-23 16:38:43 +0000

    Update main.sh

--- void-linux/main.sh
+++ void-linux/main.sh
@@ -6,7 +6,6 @@ sudo xbps-install -Sy
 echo "installing neovim"
 sudo xbps-install -y neovim exa fzf xtools git
 echo "git config"
-git config --global user.email "krls.pjds@gmail.com"
 git config --global user.name "krlspj"
 
 #echo "launch audio_config.sh"
