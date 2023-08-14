31efeba2888da1105ecf26ec1b7d371c306791e5
krlspj <krls.pjds@gmail.com>
2023-08-14 00:08:41 +0000

    install microsoft edge script

---
+++ live-env-setup/debian/install_edge.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl -fSsL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/microsoft-edge.gpg > /dev/null
+
+echo 'deb [signed-by=/usr/share/keyrings/microsoft-edge.gpg] https://packages.microsoft.com/repos/edge stable main' | sudo tee /etc/apt/sources.list.d/microsoft-edge.list
+deb [signed-by=/usr/share/keyrings/microsoft-edge.gpg] https://packages.microsoft.com/repos/edge stable main
+
+sudo apt update
+sudo apt -y install microsoft-edge-stable
