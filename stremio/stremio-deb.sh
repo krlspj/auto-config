d00b7d3ef02c0ecd4c1891993aeec4e292f60148
krlspj <krls.pjds@gmail.com>
2023-08-14 20:29:12 +0000

    update flathub instalation

--- stremio/stremio-deb.sh
+++ stremio/stremio-deb.sh
@@ -3,16 +3,19 @@ printf "============================\n"
 printf "= Starting Stremio Install =\n"
 printf "============================\n\n"
 
-cd ~/Downloads
-printf "Downloading Stremio --> Flatpak\n\n"
-wget https://dl.flathub.org/repo/appstream/com.stremio.Stremio.flatpakref
+#cd ~/Downloads
+#printf "Downloading Stremio --> Flatpak\n\n"
+#wget https://dl.flathub.org/repo/appstream/com.stremio.Stremio.flatpakref
 
-printf "installing flatpak \n"
+printf "==> Installing flatpak \n"
 sudo apt update
 sudo apt -y install flatpak
 
-printf "Installing stremio...\n\n"
+printf "==> Add flathub repository\n\n"
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+printf "==> Installing stremio...\n\n"
 flatpak install -y com.stremio.Stremio.flatpakref
-printf "==>Launch stremio from flatpak\n\n"
+printf "==> Launch stremio from flatpak\n\n"
 sleep 1;
 flatpak run com.stremio.Stremio
