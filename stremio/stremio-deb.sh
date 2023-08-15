ceb3e3e356aac0ad4708057d5de9904dfca74ff6
krlspj <krls.pjds@gmail.com>
2023-08-15 14:18:39 +0000

    fix stremio install

--- stremio/stremio-deb.sh
+++ stremio/stremio-deb.sh
@@ -15,7 +15,8 @@ printf "==> Add flathub repository\n\n"
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 printf "==> Installing stremio...\n\n"
-flatpak install -y com.stremio.Stremio.flatpakref
+#flatpak install -y com.stremio.Stremio.flatpakref
+flatpak install -y com.stremio.Stremio
 printf "==> Launch stremio from flatpak\n\n"
 sleep 1;
 flatpak run com.stremio.Stremio
