86f5d39874d6f86211110cf1f9edde8a6b91c578
krlspj <krls.pjds@gmail.com>
2023-09-24 18:51:31 +0200

    install c_formatter_42

--- 42bcn-env/install-scripts/setup.sh
+++ 42bcn-env/install-scripts/setup.sh
@@ -17,6 +17,11 @@ source $HOME/venv/bin/activate
 pip3 install --upgrade pip setuptools
 pip3 install norminette
 
+#pip3 install --user c-formatter-42
+pip3 install c-formatter-42
+echo "use c-formatter-42:"
+echo "c_formatter_42 <filename.c>"
+
 echo ">> end update and packages installation"
 
 echo ">> installing francinette"
