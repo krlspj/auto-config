9209a585ff2a26de66e4a77286f0d6c566a313ae
krlspj <43220110+krlspj@users.noreply.github.com>
2023-10-30 12:51:26 +0000

    update neovim version

--- live-env-setup/debian/neovim_appimage.sh
+++ live-env-setup/debian/neovim_appimage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p ~/bin
-wget -O ~/bin/nvim.appimage https://github.com/neovim/neovim/releases/download/v0.9.2/nvim.appimage
+wget -O ~/bin/nvim.appimage https://github.com/neovim/neovim/releases/download/v0.9.4/nvim.appimage
 chmod +x ~/bin/nvim.appimage
 
 #echo "# Neovim alias" >> ~/.zshrc
