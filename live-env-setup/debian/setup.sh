60899570fa26ccc891aa7453429cc3b80685b1e5
krlspj <43220110+krlspj@users.noreply.github.com>
2024-09-04 23:52:45 +0000

    Update setup.sh

--- live-env-setup/debian/setup.sh
+++ live-env-setup/debian/setup.sh
@@ -7,7 +7,12 @@ sudo apt-get install -y neovim
 # install packages for openvpn
 sudo apt-get -y install openvpn network-manager-openvpn network-manager-l2tp
 
+# Google Chrome
+curl -fSsL https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/google-chrome.gpg >> /dev/null
+echo deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt update && sudo apt -y install google-chrome-stable
+# call the script
+#bash chrome-repo.sh
+
 # Recording
 sudo apt-get -y install cheese simplescreenrecorder
-
-bash chrome-repo.sh
