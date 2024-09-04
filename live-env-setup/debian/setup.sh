0d428b043d791775e895c444722a115f768eed6c
krlspj <43220110+krlspj@users.noreply.github.com>
2024-09-04 22:49:52 +0000

    Update setup.sh

--- live-env-setup/debian/setup.sh
+++ live-env-setup/debian/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get install -y neovim
+
 # VPN
 # install packages for openvpn
 sudo apt-get -y install openvpn network-manager-openvpn network-manager-l2tp
