5060462e8614234ae4a084f4a762ae7b5e7fa794
krlspj <43220110+krlspj@users.noreply.github.com>
2024-09-04 22:47:32 +0000

    Update setup.sh

--- live-env-setup/debian/setup.sh
+++ live-env-setup/debian/setup.sh
@@ -7,8 +7,4 @@ sudo apt-get -y install openvpn network-manager-openvpn network-manager-l2tp
 # Recording
 sudo apt-get -y install cheese simplescreenrecorder
 
-# Google Chrome
-sudo apt-get -y install wget
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
-
+bash chrome-repo.sh
