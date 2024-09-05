4c36bc915a2eaed740ddbe9a4fb1ad6c67a98d85
krlspj <43220110+krlspj@users.noreply.github.com>
2024-09-05 16:52:12 +0000

    Create snapshot-repo.sh

---
+++ live-env-setup/debian/snapshot-repo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# to use a snapshot from the repository at this line on /etc/apt/sources.list
+# deb http://snapshot.debian.org/archive/debian/20240901T000000Z/ bookworm main non-free-firmware
+
+# cat sources.list
+##deb http://deb.debian.org/debian/ bookworm main non-free-firmware
+##deb-src http://deb.debian.org/debian/ bookworm main non-free-firmware
+##deb [trusted=yes] file:/run/live/medium bookworm main non-free-firmware
+
+#deb http://snapshot.debian.org/archive/debian/20240901T000000Z/ bookworm main non-free-firmware
+#deb [trusted=yes] file:/run/live/medium bookworm main non-free-firmware
