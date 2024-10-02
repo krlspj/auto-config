e1606d67740e202b6d6cf1cf4585656ad55ec23d
Carles Pujades <43220110+krlspj@users.noreply.github.com>
2024-10-02 04:05:20 +0200

    Update endeavourOS.sh

--- live-env-setup/archlinux/endeavourOS.sh
+++ live-env-setup/archlinux/endeavourOS.sh
@@ -4,6 +4,26 @@
 # to allow pip install packages system wide use this flag: -break-system-packages
 # pip install --user requests --break-system-packages
 echo "Configure and install all packages needed for development"
+echo "execute like: bash ./endeavourOS.sh \"2024/09/25\""
+# Define the default value for repo_date
+default_repo_date="2024/09/25"
+
+# Check if a parameter is provided
+if [ "$#" -eq 1 ]; then
+    # Check if the parameter matches the expected format (yyyy/mm/dd)
+    if [[ "$1" =~ ^[0-9]{4}/[0-9]{2}/[0-9]{2}$ ]]; then
+        repo_date="$1"
+    else
+        #echo "Invalid date format. Please use the format 'yyyy/mm/dd'. Using default value: $default_repo_date"
+        repo_date="$default_repo_date"
+    fi
+else
+    #echo "No date parameter provided. Using default value: $default_repo_date"
+    repo_date="$default_repo_date"
+fi
+
+#echo "repo_date is set to: $repo_date"
+echo $repo_date
 
 ./set_mirrolist_to_fix_date.sh
 
